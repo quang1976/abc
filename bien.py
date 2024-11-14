ec85f7efef112e33dd2790ebf57464b568b5095a
Your GitHub Usernamequang1976 <tdquangmblxag1976@gmail.com>
2024-11-14 08:03:39 +0700

    Second commit

---
+++ bien.py
@@ -0,0 +1,14 @@
+a_int = 1
+name_str = "Tô Duy Quang"
+nhietdo_float = 12.5
+danhsach_hocsinh = ['Nguyễn Huỳnh Như','Tô Nguyễn Trường Giang']
+danhsach_hocsinh_tuple = ('Nguyễn Huỳnh Như','Tô Nguyễn Trường Giang')
+danhsach_hocsinh_dict = {'name':'Nguyễn Huỳnh Như','name':'Tô Nguyễn Trường Giang'}
+# print(type(a_int))
+# print(type(name_str))
+
+# print(type(danhsach_hocsinh))
+# print(type(danhsach_hocsinh_tuple))
+# print(type(danhsach_hocsinh_dict))
+
+print(name_str.lower()[len(name_str)-4:]) #bỏ 2 ký tự cuối cùng
